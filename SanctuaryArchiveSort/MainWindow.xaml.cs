35311ad92ab1974a1964a1dbbcf9a33e3079c40f
euroll <sanctuary@sanctuary.moe>
2022-02-06 23:44:46 -0600

    reprosito ototry.

---
+++ SanctuaryArchiveSort/MainWindow.xaml.cs
@@ -0,0 +1,41 @@
+﻿using HandyControl.Themes;
+using HandyControl.Tools;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SanctuaryArchiveSort
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : HandyControl.Controls.Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string item = Guid.NewGuid().ToString().Split('-')[0];
+            xd.Items.Add(item);
+            xd.SelectedItem = item;
+        }
+
+        private void AccentButton_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
